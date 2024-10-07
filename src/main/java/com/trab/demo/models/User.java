6156e2f91d862bfc1f5504c6cee9fe6a5d4ee041
Thiago Rezende Aguiar <thiagorezende11@yahoo.com.br>
2024-10-07 18:02:36 -0300

    jpa, h2 and test profile configured

--- src/main/java/com/trab/demo/models/User.java
+++ src/main/java/com/trab/demo/models/User.java
@@ -2,7 +2,17 @@ package com.trab.demo.models;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
