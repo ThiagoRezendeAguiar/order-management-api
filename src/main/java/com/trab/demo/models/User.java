f173ed84d31db8ee1174c6ff602c8ba22be572e1
Thiago Rezende Aguiar <thiagorezende11@yahoo.com.br>
2024-10-08 15:21:06 -0300

    order added

--- src/main/java/com/trab/demo/models/User.java
+++ src/main/java/com/trab/demo/models/User.java
@@ -1,11 +1,16 @@
 package com.trab.demo.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +23,10 @@ public class User {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -68,6 +77,10 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return this.orders;
     }
 
     @Override
